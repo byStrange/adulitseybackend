970f8188bb9f329cd2b3c8c509c0a97966ed796b
Stephen Strange <qosimovrahmatullo006@gmail.com>
2023-03-30 15:29:33 +0500

    +

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path('', views.index, name="Index")
+    path('', views.index, name="Index"),
+    path('news/', views.all_news, name="all_news"),
+    path('news/<int:pk>', views.single_news_post, name="single_post")
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from . import models
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def all_news(request):
+    news = models.NewsPost.objects.all()
+    return render(request, 'main/all_news.html', {"news_posts": news})
+
+
+def single_news_post(request, pk):
+    post = models.NewsPost.objects.get(pk=pk)
+    return render(request, 'main/news.html', {'post': post})
