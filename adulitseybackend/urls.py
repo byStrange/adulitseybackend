033a6b75ec154b1b24c835acea8788048ae340c7
Stephen Strange <qosimovrahmatullo006@gmail.com>
2023-03-30 10:56:18 +0500

    update

--- adulitseybackend/urls.py
+++ adulitseybackend/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls'))
 ]
 
 
